7a9bf8c3b40bce3152900e31111f3e2ca88ae0be
JungJunho <jhchung@sogang.ac.kr>
2024-03-31 14:24:56 +0900

    Add Update in leader

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ print("Hello World 2 in team leader server")
 print("Hello World 3 in team member B local server")
 
 print("Hello World 3 in team member A local server")
+
+print("Update in leader")
